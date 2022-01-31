9ea458d2cbc257ac347f788e47bd577693cf6f72
Satyajyoti Panda <83211497+satya965-panda@users.noreply.github.com>
2022-01-31 21:56:58 +0530

    Day 1 Assignment Submitted

---
+++ assignment_day_1.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+sum = 0
+
+while True:
+  number = int(input("Enter the number :"))
+  sum += number
+  22
+
+  choice = input("Want to sum another number ? (y/n) :")
+  if choice.casefold() == 'n':
+      break;
+
+print(f"sum : {sum}")
+
+
+# In[ ]:
+
+
+
+
